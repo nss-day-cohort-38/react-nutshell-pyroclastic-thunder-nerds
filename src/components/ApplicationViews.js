801709623c6f11a31ec2bcdca154db07db2962bf
Bito Mann <bito@bitomann.com>
2020-03-06 10:06:32 -0600

    created applicationview

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,2 +1,39 @@
 import { Route, Redirect } from "react-router-dom";
 import React from "react";
+
+const ApplicationView = (props) => {
+
+    return (
+        <>
+        <Route exact path="/" render={props => {
+            return <Home />
+        }}/>
+        {/* <Route path="/welcome" render={props => {
+            return <Welcome />
+        }}/> */}
+        {/* <Route path="/login" render={props => {
+            return <Login />
+        }}/> */}
+        {/* <Route path="/logout" render={props => {
+            return <Logout />
+        }}/> */}
+        {/* <Route path="/events" render={props => {
+            return <EventList />
+        }}/> */}
+        {/* <Route path="/news" render={props => {
+            return <NewsList />
+        }}/> */}
+        {/* <Route path="/messages" render={props => {
+            return <MessageList />
+        }}/> */}
+        {/* <Route path="/tasks" render={props => {
+            return <TaskList />
+        }}/> */}
+        {/* <Route path="/friends" render={props => {
+            return <FriendList />
+        }}/> */}
+        </>
+    )
+}
+
+export default ApplicationView
