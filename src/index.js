a7f1e9c7844bbd76d1e5c9a2de034079432fbb25
Matthew Baker <mattbaker2281@gmail.com>
2020-03-06 09:39:35 -0600

    git told me to commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
     <Router>
         <Nutshell />
-    </Router>, 
-    document.getElementById("root"));
+    </Router>,document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
